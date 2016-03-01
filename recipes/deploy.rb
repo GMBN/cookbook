4f5642f3647463facdaefb1aaa3b805e5f8cb5e1
Gian Nunes <gian@btitecnologia.com>
2016-02-29 21:38:20 -0300

    Update deploy.rb

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -6,7 +6,6 @@ node[:deploy].each do |app_name, deploy|
     mode 0775
     action :create
   end
-
   
  file "#{deploy[:deploy_to]}/current/public/complemento/cidades.json" do
     mode 0775
@@ -14,5 +13,10 @@ node[:deploy].each do |app_name, deploy|
     group deploy[:group]
     action :create
   end
+  
+ execute 'permissao' do
+   command "chmod -R 775 #{deploy[:deploy_to]}/{current/public/correspondentes,current/data}"
+   action :run
+ end
   
 end
