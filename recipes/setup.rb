acdf32c4e1737db82670735d3891433a1a6d2b18
Gian Nunes <gian@btitecnologia.com>
2016-03-07 23:55:24 -0300

    Update setup.rb

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -2,7 +2,7 @@ case node['platform_family']
 when 'rhel', 'fedora'
   # centos php compiled with curl
 when 'debian'
-  package 'php5-intl' do
+  package ['php5-intl','tzdata'] do
     action :install
   end
 end
